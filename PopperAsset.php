9567783f72fc0e18e81b4ca73fbf30704be51eae
Derek Gifford <derekisbusy@gmail.com>
2019-06-23 17:14:28 -0400

    Updates

--- PopperAsset.php
+++ PopperAsset.php
@@ -5,9 +5,9 @@ use yii\web\AssetBundle;
 
 class PopperAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/derekisbusy/popper';
+    public $sourcePath = '@vendor/derekisbusy/yii2-popper';
     
     public $js = [
-        'js/popper.min.js',
+        YII_ENV_DEV ? 'js/popper.js' : 'js/popper.min.js',
     ];
 }
